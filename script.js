7dd98527d1f6c80d39c76ef99f7c23dba88c86cc
NeglectedTurnip <juxtaposedowl@gmail.com>
2019-08-30 12:42:01 -0500

    Finished the barebones of it

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 
 let userScore = 0;
 let  notUserScore = 0;
+let draw = 0;
 
 //will be used for event listers 
 let playGroup = {
   player : document.getElementById('player'),
-  computer : document.getElementById('computer')
+  computer : document.getElementById('computer'),
+  tie : document.getElementById('draw'),
+  result : document.getElementById("result")
 }
 
 //Will most likely make a smaller, objectified version of the next few functions.
@@ -16,7 +19,15 @@ let win = (userSelection, computerChoice) => {
     userScore++;
     playGroup.player.innerHTML = userScore;
     playGroup.computer.innerHTML = notUserScore;
-    console.log(`${userSelection} beats ${computerChoice}\nPlayer : ${userScore}\nComputer : ${notUserScore}\nLook at you earnin' that W. Way to go!`);
+    alert(`${userSelection} beats ${computerChoice}\nYou earned a point!`);
+}
+
+let middle = () => {
+    draw++
+    playGroup.tie.innerHTML = draw;
+    playGroup.computer.innerHTML = notUserScore;
+    playGroup.player.innerHTML = userScore;
+    alert("This is a draw!");
 }
 
 //increases the 'lose' scoreboard.
@@ -24,15 +35,18 @@ let lose = (userSelection, computerChoice) => {
     notUserScore++;
     playGroup.computer.innerHTML = notUserScore;
     playGroup.player.innerHTML = userScore;
-    console.log(`${computerChoice} beats ${userSelection}\nPlayer : ${userScore}\nComputer : ${notUserScore}\nOof...take this L, fam.`);
+    alert(`${computerChoice} beats ${userSelection}\nComputer earned a point!`);
 }
 
 //restarts scoreboard.
 let startOver = () =>{
-    userScore = notUserScore = tie = 0;
+    userScore = notUserScore = draw = 0;
     playGroup.player.innerHTML = userScore;
     playGroup.computer.innerHTML = notUserScore;
+    playGroup.tie.innerHTML = draw;
+    document.getElementById("result").innerHTML = "";
 }
+
 //Scoreboard ends
 
 //creates the RPS Standard
@@ -44,15 +58,15 @@ const weapons = {
 
  //prompts user input for game to start
 let userChoice = () => {
-        start = prompt("Rock, paper, or scissors?").toLowerCase();
-        let validResponse = ["rock", "paper", "scissors"];
-        if(validResponse.includes(start)) {
-            return start;
-        } else {
-            alert("That's not what we're looking for, friend. Try again");
-            return userChoice();
-        }
+    start = prompt("Rock, paper, or scissors?").toLowerCase();
+    let validResponse = ["rock", "paper", "scissors"];
+    if(validResponse.includes(start)) {
+        return start;
+    } else {
+        alert("That's not what we're looking for, friend. Try again");
+        return userChoice();
     }
+}
 
 //generates a random input for computer to use
 function computerChoice() {
@@ -70,20 +84,24 @@ function computerChoice() {
 let compare = (playerSelection, computerSelection) => {
     //Updates and keeps track of the score
     if(weapons[playerSelection].strongTo === computerSelection) {
-        console.log("You won this round!");
         win(playerSelection, computerSelection);
     } else if(weapons[playerSelection].weakTo === computerSelection) {
-        console.log("Computer won this round!");
         lose(playerSelection, computerSelection);
     } else {
-        console.log("This is a draw!");
+        middle();
     }
-
 }
     
 //plays the actual game with rounds.
 let playRound = (/*rounds*/) => {
-    for(i = 0; i < 5; i++){
+    while(userScore + notUserScore !== 5) {
         compare(userChoice(), computerChoice());
+    }
+    if(userScore > notUserScore){
+        console.log(`Player : ${userScore}\nComputer : ${notUserScore}\nLook at you earnin' that W. Way to go!`);
+        playGroup.result.innerHTML = "Look at you earnin' that W. Way to go!"
+    } else {
+        console.log(`Player : ${userScore}\nComputer : ${notUserScore}\nOof...take this L, fam.`);
+        playGroup.result.innerHTML = "Oof...take this L, fam."
     }
 }
