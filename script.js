d24e8f8f93caf17093d23631b5c0deb597f9b919
NeglectedTurnip <juxtaposedowl@gmail.com>
2019-08-31 14:44:56 -0500

    Better organized and cleaned the code as well as updated function names for better accessability.

--- script.js
+++ script.js
@@ -1,75 +1,62 @@
 ///next goal is to shorten the lines of code.
-
 let userScore = 0;
-let  notUserScore = 0;
+let  computerScore = 0;
 let draw = 0;
-
-//will be used for event listers 
-let playGroup = {
-  player : document.getElementById('player'),
-  computer : document.getElementById('computer'),
-  tie : document.getElementById('draw'),
-  result : document.getElementById("result")
+console.log(`Player : ${userScore}\nComputer : ${computerScore}\nEnter 'playRound()' in console to begin game, 'scoreBoard.startOver()' to restart`);
+//creates a scoreboard that directly connects 
+let scoreBoard = {
+    player : document.getElementById('player'),
+    computer : document.getElementById('computer'),
+    tie : document.getElementById('draw'),
+    result : document.getElementById("result"),
+    //increases the scoreboards through incremental operators.
+    winPoint(userChoice, computerChoice){
+        userScore++;
+        this.player.innerHTML = userScore;
+        alert(`${userChoice} beats ${computerChoice}\nYou earned a point!`);
+    },
+    drawMatch(){ 
+        draw++;
+        this.tie.innerHTML = draw;
+        alert("This is a draw!");
+    },
+    noPoint(userChoice, computerChoice){
+        computerScore++;
+        this.computer.innerHTML = computerScore;
+        alert(`${computerChoice} beats ${userChoice}\nComputer earned a point!`)
+    },
+    //sets all scores back to zero, and does away with the result <p> tag
+    startOver(){
+        userScore = computerScore = draw = 0;
+        this.player.innerHTML = userScore;
+        this.computer.innerHTML = computerScore;
+        this.tie.innerHTML = draw;
+        document.getElementById("result").innerHTML = "";
+        console.log(`Player : ${userScore}\nComputer : ${computerScore}\nEnter 'playRound()' in console to begin game, 'scoreBoard.startOver()' to restart`);
+    }
 }
 
-//Will most likely make a smaller, objectified version of the next few functions.
-
-//increases the 'player' scoreboard.
-let win = (userSelection, computerChoice) => {
-    userScore++;
-    playGroup.player.innerHTML = userScore;
-    playGroup.computer.innerHTML = notUserScore;
-    alert(`${userSelection} beats ${computerChoice}\nYou earned a point!`);
-}
-
-let middle = () => {
-    draw++
-    playGroup.tie.innerHTML = draw;
-    playGroup.computer.innerHTML = notUserScore;
-    playGroup.player.innerHTML = userScore;
-    alert("This is a draw!");
-}
-
-//increases the 'lose' scoreboard.
-let lose = (userSelection, computerChoice) => {
-    notUserScore++;
-    playGroup.computer.innerHTML = notUserScore;
-    playGroup.player.innerHTML = userScore;
-    alert(`${computerChoice} beats ${userSelection}\nComputer earned a point!`);
-}
-
-//restarts scoreboard.
-let startOver = () =>{
-    userScore = notUserScore = draw = 0;
-    playGroup.player.innerHTML = userScore;
-    playGroup.computer.innerHTML = notUserScore;
-    playGroup.tie.innerHTML = draw;
-    document.getElementById("result").innerHTML = "";
-}
-
-//Scoreboard ends
-
-//creates the RPS Standard
-const weapons = {
+//creates the RPS commands
+const rps = {
     rock: {weakTo: 'paper', strongTo: 'scissors'},
     paper: {weakTo: 'scissors', strongTo: 'Rock'},
     scissors: {weakTo: 'rock', strongTo: 'paper'}
  }
 
- //prompts user input for game to start
-let userChoice = () => {
+ //will prompt user for answer until the entry is correct. 
+function userInput(){
     start = prompt("Rock, paper, or scissors?").toLowerCase();
     let validResponse = ["rock", "paper", "scissors"];
     if(validResponse.includes(start)) {
         return start;
     } else {
         alert("That's not what we're looking for, friend. Try again");
-        return userChoice();
+        return userInput();
     }
 }
 
-//generates a random input for computer to use
-function computerChoice() {
+//generates a random input for computer to use in match.
+function genComputerInput() {
     let input = Math.floor(Math.random() * 3);
     if (input === 0) {
         return "rock";
@@ -80,28 +67,27 @@ function computerChoice() {
     }
 }
 
-//directly compares the two entries
-let compare = (playerSelection, computerSelection) => {
-    //Updates and keeps track of the score
-    if(weapons[playerSelection].strongTo === computerSelection) {
-        win(playerSelection, computerSelection);
-    } else if(weapons[playerSelection].weakTo === computerSelection) {
-        lose(playerSelection, computerSelection);
+//Updates and keeps track of the score while keeping count of draws within match.
+let compareInput = (playerSelection, computerSelection) => {
+    if(rps[playerSelection].strongTo === computerSelection) {
+        scoreBoard.winPoint(playerSelection, computerSelection);
+    } else if(rps[playerSelection].weakTo === computerSelection) {
+        scoreBoard.noPoint(playerSelection, computerSelection);
     } else {
-        middle();
+        scoreBoard.drawMatch();
     }
 }
-    
-//plays the actual game with rounds.
-let playRound = (/*rounds*/) => {
-    while(userScore + notUserScore !== 5) {
-        compare(userChoice(), computerChoice());
+
+//Will create five rounds, excluding the draws, till both user and computer will have a sum of five. 
+function playRound(){
+    while(userScore + computerScore !== 5) {
+        compareInput(userInput(), genComputerInput());
     }
-    if(userScore > notUserScore){
-        console.log(`Player : ${userScore}\nComputer : ${notUserScore}\nLook at you earnin' that W. Way to go!`);
-        playGroup.result.innerHTML = "Look at you earnin' that W. Way to go!"
+    if(userScore > computerScore){
+        console.log(`Player : ${userScore}\nComputer : ${computerScore}\nLook at you earnin' that W. Way to go!`);
+        scoreBoard.result.innerHTML = "Look at you earnin' that W. Way to go!"
     } else {
-        console.log(`Player : ${userScore}\nComputer : ${notUserScore}\nOof...take this L, fam.`);
-        playGroup.result.innerHTML = "Oof...take this L, fam.";
+        console.log(`Player : ${userScore}\nComputer : ${computerScore}\nOof...take this L, fam.`);
+        scoreBoard.result.innerHTML = "Oof...take this L, fam.";
     }
 }
